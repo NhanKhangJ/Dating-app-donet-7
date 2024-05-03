e03df53f7decc32a19413abaecef636c76078062
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2024-05-02 18:33:22 -0600

    end of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,21 +13,23 @@ export class NavComponent implements OnInit {
   @ViewChild('loginForm') loginForm: any;
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
 
   ngOnInit(): void {
   } 
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: response => {
+      next: () => this.router.navigateByUrl('/members'),
+      error: response => {
+        this.toastr.error(response.error);
         console.log(response);
-      },
-      error: error => console.log(error)
+      } 
     })
   }
 
   logout(){
     this.accountService.logOut();
+    this.router.navigateByUrl('/');
   }
 }
